b13f2d28f0c65652a52e1403e826a7cf7020e833
Vinicius <vini.domingos69@gmail.com>
2021-03-20 10:17:29 -0300

    'calculo' para solda migmag completo

--- src/components/ResultMigMag.tsx
+++ src/components/ResultMigMag.tsx
@@ -1,24 +1,44 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import { View, Text, StyleSheet, Dimensions } from 'react-native'
 
 import {PickerContext} from '../contexts/PickerContext'
+import data from '../utils/readData'
 
 export default function ResultMigMag(){
+  const [corrente, setCorrente] = useState<string>("")
+  const [tensao, setTensao] = useState<string>("")
+  const [bitola, setBitola] = useState<string>("")
+
   const {material, chapa} = useContext(PickerContext)
+  const selectedChapa = data.espessura_chapa.migmag.find(cha => cha.espessura === chapa)
+
+  useEffect(() => {
+    if(selectedChapa === null || material === null){
+      throw Error("Não foi possivel fazer a busca na matriz de dados")
+    }
+
+    const corr_cha = data.corrente_chapa.migmag.find(mm => mm.material === material && mm.chapa === selectedChapa?.id)
+    const tensaoSolda = data.tensao.find(t => t.id === corr_cha?.tensao)
+    const bitolaArarme = data.bitola.arame.find(b => b.id === corr_cha?.bitola_arame)
+
+    setBitola(String(bitolaArarme?.espessura))
+    setTensao(String(tensaoSolda?.tensao))
+    setCorrente(String(corr_cha?.corrente))
+  }, [material, chapa])
 
   return (
     <View style={styles.resultContainer}>
       <View style={styles.result}>
         <Text style={styles.resultTitle}>Corrente Média</Text>
-        <Text></Text>
+        <Text style={styles.resultText}>{corrente}</Text>
       </View>
       <View style={styles.result}>
         <Text style={styles.resultTitle}>Tensão de solda</Text>
-        <Text></Text>
+        <Text style={styles.resultText}>{tensao}</Text>
       </View>
       <View style={styles.result}>
         <Text style={styles.resultTitle}>Bitola do arame</Text>
-        <Text></Text>
+        <Text style={styles.resultText}>{bitola}</Text>
       </View>
     </View>
   )
@@ -40,5 +60,9 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     fontSize: 16,
     textAlign: "center"
+  },
+  resultText: {
+    textAlign: "center",
+    marginTop: 5
   }
 })
